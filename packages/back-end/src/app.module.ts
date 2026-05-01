2840eebb9ee0aef3b5e8f0c8da168b1945b686bd
NieWang <superexplorernw@163.com>
2026-05-01 09:57:44 +0800

    feat(webhook): dify + webhook -> github code checker

--- packages/back-end/src/app.module.ts
+++ packages/back-end/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AiModule } from './ai/ai.module';
+import { ReviewService } from './review/review.service';
+import { ReviewController } from './review/review.controller';
 
 @Module({
-  imports: [AiModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 使配置模块全局可用
+      envFilePath: '.env', // 指定 .env 文件路径
+    }),
+    AiModule,
+  ],
+  controllers: [AppController, ReviewController],
+  providers: [AppService, ReviewService],
 })
 export class AppModule {}
